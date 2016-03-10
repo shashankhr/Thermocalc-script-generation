f8f7a9638770584d742dc6c72fb39ec00f30ab95
Shashank H R <shashankhr@gmail.com>
2016-03-10 15:27:13 +0530

    Working with 4 components

---
+++ make-tcm-files_with_derivative.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+OLDIFS=$IFS
+IFS=,
+
+#This script makes the Thermocalc Macro file from inputs taken from a CSV file.
+#Make sure that the CSV file has an extra row and an extra column in the end with absolutely anything in it,
+#it will be ignored
+
+INPUT=book3.csv
+
+{
+	read T el[1] el[2] el[3] extra #reads elements from first line, which is the header. Note that the last column is ignored, and should just have something
+	while read -r T X[1] X[2] X[3] extra2
+		do
+		{
+			num=1
+			totalnum=${#el[@]}
+		
+			echo "go da"
+			echo "sw fe"
+			echo "def-sp" Ni ${el[1]} ${el[2]} ${el[3]} 
+			echo "rej p"
+			echo "*"
+			echo "rest p"
+			echo "FCC_A1"
+			echo "get_data"
+			echo "goto poly"
+			echo "s-c n=1 p=1e5 t="$T
+			echo "s-c X("${el[1]}")="${X[1]}
+			echo "s-c X("${el[2]}")="${X[2]} 
+			echo "s-c X("${el[3]}")="${X[3]} 
+			echo "c-e"
+			echo ""
+			
+			while ((num < totalnum))
+			do
+				num2=$((num+1))
+				while ((num2<=totalnum))
+				do
+					echo "s-a-v 1 X("${el[(($num))]}")" $(bc<<<"scale=10; ${X[$num]}-${X[$num]}*0.001") $(bc<<<"scale=10; ${X[$num]}+${X[$num]}*0.001") $(bc<<<"scale=10; ${X[$num]}*0.001")
+					echo "step"
+					echo ""
+					echo "enter_sym"
+					echo "fun"
+					echo "DG_"${el[$num]}"_"${el[$num2]}
+					echo "GM(FCC_A1).X("${el[$num2]}")"
+					echo ""
+					echo "enter_sym"
+					echo "tab"
+					echo "dg_table_"${el[$num]}"_"${el[$num2]}
+					echo "X("${el[$num]}") DG_"${el[$num]}"_"${el[$num2]}
+					echo ""
+					echo "tabulate dg_table_"${el[$num]}"_"${el[$num2]}
+					echo "file "${el[$num]}"_"${el[$num2]}"_data.txt"
+					echo ""
+					((num2++))
+				done
+				((num++))
+			done
+		}>$T"_with_D.tcm"
+		done
+}	< $INPUT
+IFS=$OLDIFS
